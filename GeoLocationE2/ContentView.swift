33a6db8dad20140275dc6b959c0b605eb8c541fd
KassieSanisclaws <Bytecbdeveloping@gmail.com>
2024-06-27 18:47:26 -0400

    updated to code

--- GeoLocationE2/ContentView.swift
+++ GeoLocationE2/ContentView.swift
@@ -6,56 +6,148 @@
 //
 
 import SwiftUI
-import SwiftData
+import MapKit
+
+//UserAnnotation() This request the users location soon as the app is openened and is the new way of swift 17.
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    //Locationmanager:
+    @ObservedObject var locationManager = LocationManager.shared
+    //Search Bar Location:
+    @State private var searchText = ""
+    //Create a state proper that is a new feature that swift inrtroduced i the map api called camera position.
+    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
+    //Storing the results of the search querry:
+    @State private var results = [MKMapItem]()
+    @State private var mapSelection: MKMapItem?
+    @State private var showDetails = false
+    @State private var getDirections = false
+    //Below is for routing naviating view if the polylne is on the map:
+    @State private var routeDisplaying = false
+    @State private var route: MKRoute?
+    @State private var routeDestination: MKMapItem?
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        Group {
+            if locationManager.userLocation == nil {
+                LocationRequestView()
+            } else {
+                Map(position: $cameraPosition, selection: $mapSelection) {
+                    //  Marker("My Location", systemImage: "paperplane", coordinate: .userLocation)
+                    // .tint(.green)
+                    Annotation("My Location", coordinate: .userLocation){
+                        ZStack {
+                            Circle()
+                                .frame(width: 170, height: 170)
+                                .foregroundStyle(.green.opacity(0.23))
+                            Circle()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.white)
+                            Circle()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.red)
+                        }
+                    } 
+                    //Render all the map searches from search from the resuts array.
+                    ForEach(results, id: \.self) { item in
+                        if routeDisplaying {
+                            if item == routeDestination {
+                                let placemark = item.placemark
+                                Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                            }
+                        } else {
+                            let placemark = item.placemark
+                            Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                        }
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    if let route {
+                        //New feature we get from the updated MapKit UI:
+                        MapPolyline(route.polyline)
+                            .stroke(.red, lineWidth: 6)
                     }
+                    
                 }
             }
-        } detail: {
-            Text("Select an item")
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+        
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+
+//Content view of the search:
+extension ContentView{
+    func searchPlaces() async {
+        let requests = MKLocalSearch.Request()
+        requests.naturalLanguageQuery = searchText
+        //This specifies the region you re doing the search request on.
+        requests.region = .userRegion
+        //Stores the result in the below property.
+        let results = try? await MKLocalSearch(request: requests).start()
+        self.results = results?.mapItems ?? []
+        
+    }
+  //Function gets the route & displays on screen:
+    func fetchRoute() {
+        if let mapSelection {
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: .init(coordinate: .userLocation))
+            request.destination = mapSelection
+            
+            Task {
+                let result = try? await MKDirections(request: request).calculate()
+                route = result?.routes.first
+                routeDestination = mapSelection
+                
+                withAnimation(.snappy){
+                    routeDisplaying = true
+                    showDetails = false
+                    
+                    if let rect = route?.polyline.boundingMapRect, routeDisplaying {
+                        cameraPosition = .rect(rect)
+                    }
+                }
+            }
+        }
+    }
+    
 }
+
+struct MapUserLocationButton: View {
+    @ObservedObject var locationManager: LocationManager
+    
+    var body: some View {
+        Button(action: {
+            DispatchQueue.main.async {
+                locationManager.requestLocation()
+            }
+        }) {
+            Image(systemName: "location.circle.fill")
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.green)
+        }
+        .padding()
+    }
+}
+
+//Setting user location in the map region.
+extension CLLocationCoordinate2D {
+    static let userLocation = CLLocationCoordinate2D(latitude: 43.651070, longitude: -79.347015)
+}
+
+//Creating a map view region that uses the user location
+extension MKCoordinateRegion {
+    static var userRegion: MKCoordinateRegion{
+        return .init(center: .userLocation,
+                     latitudinalMeters: 10000,
+                     longitudinalMeters: 10000)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+     
